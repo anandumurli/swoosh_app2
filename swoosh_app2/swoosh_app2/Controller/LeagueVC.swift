7e814078beccf52d595bea0b1af3fecc2ec5c11c
Peter Paul <peterpaularasu@gmail.com>
2019-02-25 22:43:26 +0530

    Sixt Change

---
+++ swoosh_app2/swoosh_app2/Controller/LeagueVC.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVCViewController.swift
+//  swoosh_app2
+//
+//  Created by Peter Paul on 25/02/19.
+//  Copyright © 2019 Emilda pvt. ltd. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
